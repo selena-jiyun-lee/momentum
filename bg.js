cbb573eda1e4e2e0505a4610d233fe3e91f63dc4
Jiyun Lee <selena.jy.lee@gmail.com>
2020-10-18 20:43:59 +0100

    Update background images

--- bg.js
+++ bg.js
@@ -1,23 +1,40 @@
 const body = document.querySelector("body");
 
+const BG_API_KEY = "vuojNA21BCqRoxut92dKZ_kGPKnsqlpaIKkNHKL9BbU";
 const IMG_NUMBER = 3;
 
-function paintImage(imgNumber) {
+
+function loadImage() {
+    fetch(`https://api.unsplash.com/photos/random?client_id=${BG_API_KEY}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        // console.log(json.urls.full);
+        const imgUrl = json.urls.full;
+        paintImage(imgUrl);
+    });
+}
+
+// function paintImage(imgNumber) {
+function paintImage(url) {
     const image = new Image();
-    image.src = `/images/background${imgNumber + 1}.jpg`;
+    // image.src = `/images/background${imgNumber + 1}.jpg`;
+    image.src = url;
     image.classList.add("bgImage");
     body.appendChild(image);
 
 }
 
-function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
-}
+// function genRandom() {
+//     const number = Math.floor(Math.random() * IMG_NUMBER);
+//     return number;
+// }
 
 function init() {
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+    // const randomNumber = genRandom();
+    // paintImage(randomNumber);
+    loadImage();
 }
 
 init();
